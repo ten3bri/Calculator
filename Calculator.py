5924a01fcbc34b70d99c368e1e34db09d9fc4530
tenebri <wojtek12900@gmail.com>
2025-04-08 21:59:47 +0200

    Add label frame and customize hover colors for buttons

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,27 @@
 from math import *
 import tkinter as tk
+#from tkinter import colorchooser
 
 class Calculator:
     def __init__(self, root):
         self.root = root
         self.root.title("Calculator")
+
+        # wybór koloru tła okna przez użytkownika
+        #chosen_color = colorchooser.askcolor(title="Wybierz kolor tła")
+        #if chosen_color[1]:
+        #    self.root.configure(bg=chosen_color[1])
+
+        self.digit_color = "#f0f0f0"
+        self.operator_color = "#cce6ff"
+        self.equal_color = "#66b3ff"
+
+        # słownik
+        self.hover_colors = {
+            self.digit_color: "#e0e0e0",
+            self.operator_color: "#b3daff",
+            self.equal_color: "#4da6ff"
+        }
 
         window_width = 300
         window_height = 400
@@ -32,8 +49,8 @@ class Calculator:
             self.root.grid_columnconfigure(j, weight=1)
 
         # Etykieta do wyświetlania wyniku z automatycznym aktualizowaniem
-        result_label = tk.Label(self.root, textvariable=self.result_var, height=1, anchor='e', font=("Arial", 15))
-        result_label.grid(row=0, column=0, columnspan=5, sticky='nsew')
+        result_label = tk.Label(self.root, textvariable=self.result_var, height=1, anchor='e', font=("Arial", 15), borderwidth=3, relief='ridge')
+        result_label.grid(row=0, column=0, columnspan=5, sticky='nsew', padx=5, pady=3)
 
         buttons = [
             ('7', 2, 1), ('8', 2, 2), ('9', 2, 3), ('/', 2, 4),
@@ -43,45 +60,66 @@ class Calculator:
         ]
 
         for (text, row, col) in buttons:
-            button = tk.Button(self.root, text=text, width=4, height=2, command = lambda t = text: self.on_button_click(t))
+            if text in '0123456789.':
+                bg_color = self.digit_color
+            elif text == '=':
+                bg_color = self.equal_color
+            else:
+                bg_color = self.operator_color
+
+            button = tk.Button(self.root, text=text, width=4, height=2, bg=bg_color, command= lambda t = text: self.on_button_click(t))
             button.grid(row=row, column=col, sticky='nsew')
+
+            # zapamiętujemy oryginalny kolor przycisku
+            button.original_bg = bg_color
+
+            # dodajemy efekty hovera
             button.bind('<Enter>', self.on_enter)
             button.bind('<Leave>', self.on_leave)
 
-        clear_all_button = tk.Button(self.root, text='AC', width=4, height=2, command = self.clear)
+        clear_all_button = tk.Button(self.root, text='AC', width=4, height=2, bg=self.operator_color, command = self.clear)
         clear_all_button.grid(row=1, column=4, sticky='nsew')
+        clear_all_button.original_bg = self.operator_color
         self.add_hover_effect(clear_all_button)
 
-        clear_last_button = tk.Button(self.root, text='CE', width=4, height=2, command = self.clear_last)
+        clear_last_button = tk.Button(self.root, text='CE', width=4, height=2, bg=self.operator_color, command = self.clear_last)
         clear_last_button.grid(row=1, column=3, sticky='nsew')
+        clear_last_button.original_bg = self.operator_color
         self.add_hover_effect(clear_last_button)
 
-        l_bracket_button = tk.Button(self.root, text='(', width=4, height=2, command = lambda: self.on_button_click('('))
+        l_bracket_button = tk.Button(self.root, text='(', width=4, height=2, bg=self.operator_color, command = lambda: self.on_button_click('('))
         l_bracket_button.grid(row=1, column=1, sticky='nsew')
+        l_bracket_button.original_bg = self.operator_color
         self.add_hover_effect(l_bracket_button)
 
-        r_bracket_button = tk.Button(self.root, text=')', width=4, height=2, command=lambda: self.on_button_click(')'))
+        r_bracket_button = tk.Button(self.root, text=')', width=4, height=2, bg=self.operator_color, command=lambda: self.on_button_click(')'))
         r_bracket_button.grid(row=1, column=2, sticky='nsew')
+        r_bracket_button.original_bg = self.operator_color
         self.add_hover_effect(r_bracket_button)
 
-        percent_button = tk.Button(self.root, text='%', width=4, height=2, command=lambda: self.on_button_click('%'))
+        percent_button = tk.Button(self.root, text='%', width=4, height=2, bg=self.operator_color, command=lambda: self.on_button_click('%'))
         percent_button.grid(row=1, column=0, sticky='nsew')
+        percent_button.original_bg = self.operator_color
         self.add_hover_effect(percent_button)
 
-        ln_button = tk.Button(self.root, text='ln', width=4, height=2, command=self.calculate_ln)
+        ln_button = tk.Button(self.root, text='ln', width=4, height=2, bg=self.operator_color, command=self.calculate_ln)
         ln_button.grid(row=2, column=0, sticky='nsew')
+        ln_button.original_bg = self.operator_color
         self.add_hover_effect(ln_button)
 
-        log_button = tk.Button(self.root, text='log', width=4, height=2, command=self.calculate_log)
+        log_button = tk.Button(self.root, text='log', width=4, height=2, bg=self.operator_color, command=self.calculate_log)
         log_button.grid(row=3, column=0, sticky='nsew')
+        log_button.original_bg = self.operator_color
         self.add_hover_effect(log_button)
 
-        sqrt_button = tk.Button(self.root, text='√', width=4, height=2, command=self.calculate_sqrt)
+        sqrt_button = tk.Button(self.root, text='√', width=4, height=2, bg=self.operator_color, command=self.calculate_sqrt)
         sqrt_button.grid(row=4, column=0, sticky='nsew')
+        sqrt_button.original_bg = self.operator_color
         self.add_hover_effect(sqrt_button)
 
-        power_button = tk.Button(self.root, text='xʸ', width=4, height=2, command=lambda: self.on_button_click('^'))
+        power_button = tk.Button(self.root, text='xʸ', width=4, height=2, bg=self.operator_color, command=lambda: self.on_button_click('^'))
         power_button.grid(row=5, column=0, sticky='nsew')
+        power_button.original_bg = self.operator_color
         self.add_hover_effect(power_button)
 
     def on_button_click(self, char):
@@ -159,11 +197,13 @@ class Calculator:
 
     # przy najechaniu myszką zmiana koloru przycisku
     def on_enter(self, event):
-        event.widget['background'] = 'lightgray'
+        original_color = event.widget.cget("bg")
+        hover_color = self.hover_colors.get(original_color, original_color)
+        event.widget['background'] = hover_color
 
     # z powrotem na domyślny kolor
     def on_leave(self, event):
-        event.widget['background'] = 'SystemButtonFace'
+        event.widget['background'] = event.widget.original_bg
 
     def add_hover_effect(self, button):
         button.bind("<Enter>", self.on_enter)
